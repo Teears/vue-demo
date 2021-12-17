42b78de56b92173371760dc1821bff9865be9bf7
Teears <2734189761@qq.com>
2021-12-17 22:15:47 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -17,12 +16,21 @@ export default {
 </script>
 
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+}
+html,body{
+  width: 100%;
+  height: 100%;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,243 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="myChart" id="myChart" ref="myChart"></div>
   </div>
 </template>
 
 <script>
+import china from "echarts/map/json/china.json";
+import world from "echarts/map/json/world.json";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      option: {
+        geo: {
+          map: "world",
+          type: "map",
+          center: [114.07, 22.62],
+          boundingCoords: [
+            [64, 45],
+            [120, -20]
+          ],
+          aspectScale: 1,
+          selectedMode: "single",
+          label: {
+            normal: {
+              show: false
+            },
+            emphasis: {
+              show: false
+            }
+          },
+          itemStyle: {
+            normal: {
+              borderWidth: 1,
+              borderColor: "#69b0fe",
+              areaColor: "#04388c",
+              label: {
+                show: false
+              }
+            },
+            emphasis: {
+              borderWidth: 1,
+              borderColor: "#69b0fe",
+              areaColor: "#04388c",
+              label: {
+                show: false
+              }
+            }
+          },
+          regions: [
+            {
+              name: "四川",
+              itemStyle: {
+                normal: {
+                  borderWidth: 1,
+                  borderColor: "#2affeb",
+                  areaColor: "#05365f",
+                  label: {
+                    show: false
+                  }
+                },
+                emphasis: {
+                  borderWidth: 2,
+                  borderColor: "#ffffff",
+                  areaColor: "#04388c",
+                  shadowColor: "#04aaff",
+                  shadowBlur: 20,
+                  label: {
+                    show: false
+                  }
+                }
+              }
+            },
+            {
+              name: "广东",
+              itemStyle: {
+                normal: {
+                  borderWidth: 1,
+                  borderColor: "#2affeb",
+                  areaColor: "#05365f",
+                  label: {
+                    show: false
+                  }
+                },
+                emphasis: {
+                  borderWidth: 2,
+                  borderColor: "#ffffff",
+                  areaColor: "#04388c",
+                  shadowColor: "#04aaff",
+                  shadowBlur: 20,
+                  label: {
+                    show: false
+                  }
+                }
+              }
+            },
+            {
+              name: "Malaysia",
+              itemStyle: {
+                normal: {
+                  borderWidth: 1,
+                  borderColor: "#2affeb",
+                  areaColor: "#05365f",
+                  label: {
+                    show: false
+                  }
+                },
+                emphasis: {
+                  borderWidth: 2,
+                  borderColor: "#ffffff",
+                  areaColor: "#04388c",
+                  shadowColor: "#04aaff",
+                  shadowBlur: 20,
+                  label: {
+                    show: false
+                  }
+                }
+              }
+            },
+            {
+              name: "Vietnam",
+              itemStyle: {
+                normal: {
+                  borderWidth: 1,
+                  borderColor: "#2affeb",
+                  areaColor: "#05365f",
+                  label: {
+                    show: false
+                  }
+                },
+                emphasis: {
+                  borderWidth: 2,
+                  borderColor: "#ffffff",
+                  areaColor: "#04388c",
+                  shadowColor: "#04aaff",
+                  shadowBlur: 20,
+                  label: {
+                    show: false
+                  }
+                }
+              }
+            }
+          ]
+        },
+        series: {
+          type: "scatter",
+          coordinateSystem: "geo",
+          symbol: "../../static/po.png",
+          itemStyle: {
+            color: "#b02a02"
+          },
+          data: [
+            [104.06, 30.67, 50],
+            [114.07,22.62, 50],
+          ]
+        }
+      }
+    };
+  },
+  methods: {
+    decodePolygon(coordinate, encodeOffsets, encodeScale) {
+      var result = [];
+      var prevX = encodeOffsets[0];
+      var prevY = encodeOffsets[1];
+      for (var i = 0; i < coordinate.length; i += 2) {
+        var x = coordinate.charCodeAt(i) - 64;
+        var y = coordinate.charCodeAt(i + 1) - 64;
+        // ZigZag decoding
+        x = (x >> 1) ^ -(x & 1);
+        y = (y >> 1) ^ -(y & 1);
+        // Delta deocding
+        x += prevX;
+        y += prevY;
+        prevX = x;
+        prevY = y;
+        // Dequantize
+        result.push([x / encodeScale, y / encodeScale]);
+      }
+      return result;
+    },
+    decode(json) {
+      if (!json.UTF8Encoding) {
+        return json;
+      }
+      var encodeScale = json.UTF8Scale;
+      if (encodeScale == null) {
+        encodeScale = 1024;
+      }
+      var features = json.features;
+      for (var f = 0; f < features.length; f++) {
+        var feature = features[f];
+        var geometry = feature.geometry;
+        var coordinates = geometry.coordinates;
+        var encodeOffsets = geometry.encodeOffsets;
+        for (var c = 0; c < coordinates.length; c++) {
+          var coordinate = coordinates[c];
+          if (geometry.type === "Polygon") {
+            coordinates[c] = this.decodePolygon(
+              coordinate,
+              encodeOffsets[c],
+              encodeScale
+            );
+          } else if (geometry.type === "MultiPolygon") {
+            for (var c2 = 0; c2 < coordinate.length; c2++) {
+              var polygon = coordinate[c2];
+              coordinate[c2] = this.decodePolygon(
+                polygon,
+                encodeOffsets[c][c2],
+                encodeScale
+              );
+            }
+          }
+        }
+      }
+      // Has been decoded
+      json.UTF8Encoding = false;
+      return json;
+    }
+  },
+  mounted() {
+    let data = this.decode(china);
+    console.log(world);
+    console.log(data);
+    let worldAndChina = Object.assign({}, world); // 原本的world还需要用 所以这里用了一个深拷贝赋值世界地图数据
+    worldAndChina.features = worldAndChina.features.concat(data.features); // 对，就是这么简单用concat把两者的features合并起来就可以了
+    var chart = this.$echarts.init(this.$refs.myChart);
+    this.$echarts.registerMap("world", worldAndChina);
+    chart.setOption(this.option);
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello {
+  height: 100%;
+  width: 100%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.myChart {
+  height: 100%;
+  width: 100%;
 }
 </style>
